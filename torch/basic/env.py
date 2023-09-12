082e972142aa349c508e64c768a73bc8bec5c527
scaredmonad <voidptr9@gmail.com>
2023-09-12 21:49:52 +0300

    chore: clean slate

--- torch/basic/env.py
+++ torch/basic/env.py
@@ -1,6 +1,19 @@
-try:
+try: # sanity check
   import torch
   x = torch.rand(5, 3)
   print(x)
-except:
-  pass
+except ReferenceError:
+  exit()
+
+import torch
+import numpy as np
+
+# From raw data
+data = [[1, 2],[3, 4]]
+x_data = torch.tensor(data)
+print("raw:",{x_data, x_data.shape, x_data.dtype})
+
+# From np ndarray
+np_array = np.array(data)
+x_np = torch.from_numpy(np_array)
+print("array is tensor: ", isinstance(x_np, torch.Tensor))
